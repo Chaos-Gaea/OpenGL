a6d067fe8c59fc01d90ac05cc9ce6bb088d48b63
resouse <570802355@qq.com>
2019-03-11 15:01:21 +0800

    OpenGL简单实现

---
+++ OpenGlAngle/AngelActivity.java
@@ -0,0 +1,54 @@
+package lyp.com.text.OpenGlAngle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by lyp on 2019/3/6.
+ */
+
+public class AngelActivity extends Activity {
+
+    private MyGLSurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        mGLSurfaceView = new MyGLSurfaceView(this);
+        setContentView(mGLSurfaceView);
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
+        mGLSurfaceView.requestFocus();//获取焦点
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+
+
+
+}
